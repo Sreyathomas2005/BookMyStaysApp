c5d380ee916701c67abd2cc2c98cd927bc9d97ed
Sreyathomas2005 <sreyacthomas@gmail.com>
2026-04-01 15:13:21 +0530

    Sreaythomas2005 Added UC1

---
+++ App/src/BookMyStaysApp.java
@@ -0,0 +1,13 @@
+
+public class BookMyStaysApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Welcome to Book My Stay App ===");
+
+        System.out.println("Hotel Booking Management System v1.0");
+
+        System.out.println("Application started successfully.");
+
+    }
+}
